7e07598e67305cff859b2e9bde635ec9dbf4d2cc
Mawil Hasan <mawil0721@berkeley.edu>
2022-03-06 19:27:15 -0800

    added scroll top component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './About';
 import Timeline from './Timeline';
 import Projects from './Projects';
 import Contact from './Contact';
+import Footer from './Footer';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Timeline />
       <Projects/>
       <Contact />
+      {/* <Footer /> */}
 
     </div>
   );
